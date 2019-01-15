78d693bb015b9c6c1c7a9bd7856d8bcfb8a8cc26
auscoi <auscoine@gmail.com>
2019-01-15 18:10:10 -0500

    ...

--- redesign/classes/class_BlockCrawler.php
+++ redesign/classes/class_BlockCrawler.php
@@ -365,12 +365,12 @@ class BlockCrawler {
     {
       if ($query == Null) {$this->error("no_hash"); return FALSE;}
       $raw_block = $this->WalletRPC->getblock($query);
-      if ($$raw_block["time"] == Null) { return $this->error("invalid_hash"); }
+      if ($raw_block["time"] == Null) { return $this->error("invalid_hash"); }
     } else { 
       if ($query == Null) {$this->error("no_height"); return FALSE;}
       $block_hash = $this->WalletRPC->getblockhash(intval($query));
       $raw_block = $this->WalletRPC->getblock($block_hash);
-      if ($$raw_block["time"] == Null) { return $this->error("invalid_height"); }
+      if ($raw_block["time"] == Null) { return $this->error("invalid_height"); }
     }
 
     
