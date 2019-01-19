ba0919bf64add0d7cb08e7e23c603888d45ca199
Auscoine <auscai@MacBook-Pro.local>
2019-01-19 18:04:02 -0500

    ...

--- classes/class_BlockCrawler.php
+++ classes/class_BlockCrawler.php
@@ -742,7 +742,7 @@ foreach ($raw_tx["vout"] as $key => $txout)
 {
   $output = $this->Block2Redis->build_output($txout);
   echo $output["key"].'<br>';
-  echo $output["value"];
+  var_dump(json_decode($output["value"], true));
 
   array_push($html, '
     <div class="row">
