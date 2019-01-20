2b0a21861dafc63f9bd75b918184241d3a9bdf79
Auscoine <auscai@MacBook-Pro.local>
2019-01-19 20:52:03 -0500

    ...

--- classes/class_Block2Redis.php
+++ classes/class_Block2Redis.php
@@ -132,17 +132,19 @@ class Block2Redis {
 	    while ($this->height < $this->blockchaininfo["blocks"]) 
 	    {
 	    	echo "Block ".$this->height."<br>";
-			/*
+			
 			$block_hash = $this->WalletRPC->getblockhash(intval($this->height));
 			$raw_block = $this->WalletRPC->getblock($block_hash);
-			
-			if (! $raw_block) { return $this->error("invalid_height"); }
-
 			$new_block = $this->build_block($raw_block);
-			*/
+			
+			var_dump(json_decode($new_block["data"], TRUE));
 
+			
+			if ($this->height == 10): break;
 
 			$this->height++;
+
+			
 
 
 	    }	
