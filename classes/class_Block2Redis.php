20ef589bff2ee727bce71f4693649ea2dc815558
Auscoine <auscai@MacBook-Pro.local>
2019-01-20 00:02:07 -0500

    ...

--- classes/class_Block2Redis.php
+++ classes/class_Block2Redis.php
@@ -122,7 +122,7 @@ class Block2Redis {
 	   	if ($this->Redis->exists($this->RKEY)) {
 
 	   		// get array of REDIS hkeys matching "block::*" (maybe using hScan)	
-	   		$keys = $this->Redis->hkeys($this->RKEY);
+	   		$keys = $this->Redis->hKeys($this->RKEY);
 	   		foreach ($keys as $k => $v)
 	   		{
 	   			// copy to new array parsing out "block::"
