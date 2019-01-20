d1bf7ee7d4925d82cf8d61bc3fbf09e76e65154e
Auscoine <auscai@MacBook-Pro.local>
2019-01-19 19:04:41 -0500

    ...

--- classes/class_BlockCrawler.php
+++ classes/class_BlockCrawler.php
@@ -71,8 +71,8 @@ class BlockCrawler {
     $this->networkhashps  = $this->WalletRPC->getnetworkhashps();
     $this->site_content   = $this->show_dashboard();
 
-    //require_once ("class_Block2Redis.php");
-    //$this->Block2Redis = new Block2Redis($rpc_user, $rpc_pass, $rpc_addy, $rpc_port);
+    require_once ("class_Block2Redis.php");
+    $this->Block2Redis = new Block2Redis($rpc_user, $rpc_pass, $rpc_addy, $rpc_port);
   }
 
   // Rounding to chopping too many decimal places (i.e. difficulty)
