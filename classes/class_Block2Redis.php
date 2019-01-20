059669201b6e8d7830da9fed4f5bc5460bb67aef
Auscoine <auscai@MacBook-Pro.local>
2019-01-20 00:43:09 -0500

    ..

--- classes/class_Block2Redis.php
+++ classes/class_Block2Redis.php
@@ -207,7 +207,8 @@ class Block2Redis {
 		// update db height value
 		$this->Redis->hSet($this->RKEY, "height", $raw_block["height"]);
 
-		echo "<hr>".$this->Redis-hGet($this->RKEY, $rdata["key"]);
+		$block_data = $this->Redis-hGet($this->RKEY, $rdata["key"]);
+		echo "<hr>".$block_data;
 	}
 
 	// assemble a new transaction to insert
